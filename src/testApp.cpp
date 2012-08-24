f28428d8d63bcf7a3cfd9ad87c88a37f6d3fa521
Joan Perals Tresserra <j.pertres@gmail.com>
2012-08-25 01:56:47 +0200

    Copied some useful code from Mappero. Added license accordingly

--- src/testApp.cpp
+++ src/testApp.cpp
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2012 Joan Perals Tresserra
+ *
+ * This software is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this software.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #include "testApp.h"
 #include <sqlite3.h>
 #include <stdio.h>
@@ -24,6 +40,69 @@ ofTrueTypeFont 	timeFont;
 int prev_x = 0, prev_y = 0, prev_t = 0;
 int currentSpeed = 0, maxSpeed = 0, maxSpeedHalf, maxSpeedQuarter;
 ofPoint windowDimensions;
+
+/* START Code copied from Mappero */
+
+#define deg2rad(deg) ((deg) * (PI / 180.0))
+#define rad2deg(rad) ((rad) * (180.0 / PI))
+
+#ifdef USE_DOUBLES_FOR_LATLON
+#define GSIN(x) sin(x)
+#define GCOS(x) cos(x)
+#define GASIN(x) asin(x)
+#define GTAN(x) tan(x)
+#define GATAN(x) atan(x)
+#define GATAN2(x, y) atan2(x, y)
+#define GEXP(x) exp(x)
+#define GLOG(x) log(x)
+#define GPOW(x, y) pow(x, y)
+#define GSQTR(x) sqrt(x)
+#else
+#define GSIN(x) sinf(x)
+#define GCOS(x) cosf(x)
+#define GASIN(x) asinf(x)
+#define GTAN(x) tanf(x)
+#define GATAN(x) atanf(x)
+#define GATAN2(x, y) atan2f(x, y)
+#define GEXP(x) expf(x)
+#define GLOG(x) logf(x)
+#define GPOW(x, y) powf(x, y)
+#define GSQTR(x) sqrtf(x)
+#endif
+
+#define MAX_ZOOM (20)
+#define TILE_SIZE_P2 (8)
+
+#define WORLD_SIZE_UNITS (2 << (MAX_ZOOM + TILE_SIZE_P2))
+
+#define MERCATOR_SPAN (-6.28318377773622)
+#define MERCATOR_TOP (3.14159188886811)
+
+#ifdef USE_DOUBLES_FOR_LATLON
+typedef gdouble MapGeo;
+#else
+typedef gfloat MapGeo;
+#endif
+
+void unit2latlon_google(gint unitx, gint unity, MapGeo *lat, MapGeo *lon)
+{
+    MapGeo tmp;
+    *lon = (unitx * (360.0 / WORLD_SIZE_UNITS)) - 180.0;
+    tmp = (unity * (MERCATOR_SPAN / WORLD_SIZE_UNITS)) + MERCATOR_TOP;
+    *lat = (360.0 * (GATAN(GEXP(tmp)))) * (1.0 / PI) - 90.0;
+}
+
+void latlon2unit_google(MapGeo lat, MapGeo lon, gint *unitx, gint *unity)
+{
+    MapGeo tmp;
+
+    *unitx = (lon + 180.0) * (WORLD_SIZE_UNITS / 360.0) + 0.5;
+    tmp = GSIN(deg2rad(lat));
+    *unity = 0.5 + (WORLD_SIZE_UNITS / MERCATOR_SPAN) *
+        (GLOG((1.0 + tmp) / (1.0 - tmp)) * 0.5 - MERCATOR_TOP);
+}
+
+/* END Code copied from Mappero */
 
 void rescalePoint() {
     for(int i = 0; i < pointHeight; i++) {
